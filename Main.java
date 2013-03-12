2bc9ae0b1bfbe2056d1845ad1c127ac79c6bd146
Ali <iamaliferg@gmail.com>
2013-03-12 23:33:44 +0000

    added my section to latex

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static int time = 200;
     
 	public static void main(String[] args) {
-		int[] arr = {4,3,8,1,2,12};
+		int[] arr = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
 		anim = new AnimatedArray(arr);
 		/*setup thread for creating GUI */
 		
