14d5fa2e607444a5eadfd99da54236b35930f231
Andrew Gibson <andrewjgibson@hotmail.co.uk>
2013-03-15 22:20:58 +0000

    updated issues, contribution, conclusion (draft)

---
+++ AnimatedLinkedList/testAnimatedLinkedList.java
@@ -0,0 +1,15 @@
+package AnimatedLinkedList;
+
+
+
+/*a class that instantiates an AnimatedLinkedList object and tests its functionality
+ */
+
+public class testAnimatedLinkedList {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
